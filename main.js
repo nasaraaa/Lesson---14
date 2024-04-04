0d77c04a9a0495844195bd868bf3f0173ad821fa
nasaraaa <159721509+nasaraaa@users.noreply.github.com>
2024-04-05 02:06:30 +0400

    js

---
+++ main.js
@@ -0,0 +1,28 @@
+let numbers = [2005 , 2006 , 2007, 2008 , 2009];
+let total = numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4];
+let all = numbers.length
+console.log(all)
+console.log(numbers);
+console.log(total);
+
+
+let massivs = [
+    {name:`Tornike` , age: 18 },
+    {name:`Tornike1` , age: 19},
+    {name:`Tornike2` , age: 20},
+    {name:`Tornike3` , age: 21},
+    {name:`Tornike4` , age: 22},
+]
+console.log(massivs)
+
+const personal = {
+    firstName: `Tornike`,
+    lastName: `nasaridze`,
+    addres:[`chuninashvilis4`, `pirosmanis29`],
+    age:18,
+    PhoneNumber:[599105078 ,595525530]
+
+}
+console.log(personal);
+
+console.log(` my name is ${personal.firstName}, my age is ${personal.age}, my addres is ${personal.addres[0]}`)
